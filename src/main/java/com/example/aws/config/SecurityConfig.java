ac996d5d73cf50ee9d0555beadabd6ea1251f1d6
khanhnd <khanhnd@timebird.org>
2021-12-09 14:55:20 +0700

    demo jwt

--- src/main/java/com/example/aws/config/SecurityConfig.java
+++ src/main/java/com/example/aws/config/SecurityConfig.java
@@ -1,37 +1,37 @@
-package com.example.aws.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.servlet.http.HttpServletResponse;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-  public static final String[] pathArray = new String[] { "/api/v1/system/**", "/swagger-ui.html", "/v2/api-docs",
-    "/swagger-resources/**", "/webjars/**", "/api/v1/auth/**", "/api/v1/firebase/unsubscribe-device" };
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.cors().and().httpBasic().disable().csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**")
-      .anonymous().antMatchers(pathArray).permitAll().anyRequest().authenticated().and().exceptionHandling()
-      .authenticationEntryPoint(unauthorizedEntryPoint()).and().sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.STATELESS).
-      and().apply(new JwtConfigurer(jwtTokenProvider));
-  }
-
-  @Bean
-  public AuthenticationEntryPoint unauthorizedEntryPoint() {
-    return (request, response, authException) -> response
-      .sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-  }
-}
+//package com.example.aws.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.AuthenticationEntryPoint;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import javax.servlet.http.HttpServletResponse;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//  public static final String[] pathArray = new String[] { "/api/v1/system/**", "/swagger-ui.html", "/v2/api-docs",
+//    "/swagger-resources/**", "/webjars/**", "/api/v1/auth/**", "/api/v1/firebase/unsubscribe-device" };
+//
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http.cors().and().httpBasic().disable().csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**")
+//      .anonymous().antMatchers(pathArray).permitAll().anyRequest().authenticated().and().exceptionHandling()
+//      .authenticationEntryPoint(unauthorizedEntryPoint()).and().sessionManagement()
+//      .sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+//      and().apply(new JwtConfigurer(jwtTokenProvider));
+//  }
+//
+//  @Bean
+//  public AuthenticationEntryPoint unauthorizedEntryPoint() {
+//    return (request, response, authException) -> response
+//      .sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+//  }
+//}
--- src/main/java/com/example/aws/controller/UserController.java
+++ src/main/java/com/example/aws/controller/UserController.java
@@ -14,11 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class UserController {
 
   @Autowired
